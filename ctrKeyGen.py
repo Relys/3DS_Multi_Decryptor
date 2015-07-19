b942d6444ef6eb425a06acd64e16a95bf6fd73cd
mixups <weigom@gmx.com>
2015-07-19 21:08:54 +0900

    Fixed Seed Crypto support
    Fixed the issue in Seed Crypto not uses 7.x NCCH crypto.
    For Pony Pets 3D (Japan).

--- ctrKeyGen.py
+++ ctrKeyGen.py
@@ -250,7 +250,7 @@ def parseNCCH(fh, offs=0, idx=0, titleId='', standAlone=1):
 	useSeedCrypto = header.flags[7] == 32
 	if useSeedCrypto:
 		keyY = getNewkeyY(keyY,header,struct.pack('I',int(titleId[8:],16))+struct.pack('I',int(titleId[:8],16)))
-		print tab + 'Use Seed NCCH crypto'
+		print tab + 'Uses Seed NCCH crypto'
 		print tab + 'Seed KeyY: %s' % hexlify(keyY).upper()
 	
 	print ''
@@ -265,6 +265,10 @@ def parseNCCH(fh, offs=0, idx=0, titleId='', standAlone=1):
 		data = data + genOutName(titleId, ncsdPartitions[idx], b'exefs_norm')
 		entries += 1
 		if uses7xCrypto:
+			data = data + parseNCCHSection(header, ncchSection.exefs, uses7xCrypto, useSeedCrypto, 0, tab)
+			data = data + genOutName(titleId, ncsdPartitions[idx], b'exefs_7x')
+			entries += 1
+		elif useSeedCrypto and not uses7xCrypto:
 			data = data + parseNCCHSection(header, ncchSection.exefs, uses7xCrypto, useSeedCrypto, 0, tab)
 			data = data + genOutName(titleId, ncsdPartitions[idx], b'exefs_7x')
 			entries += 1
